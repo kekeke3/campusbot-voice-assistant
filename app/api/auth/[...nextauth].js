9b5aae46120ae5812b84a6a0bc8e15e680e676b2
Gerald C. Manatad <gerald.manatad@evsu.edu.ph>
2025-04-13 20:44:45 +0800

    intial setup for prisma, mongodb, next auth js

---
+++ app/api/auth/[...nextauth].js
@@ -0,0 +1,31 @@
+// import NextAuth from "next-auth"
+// import GithubProvider from "next-auth/providers/github"
+
+// export const authOptions = {
+//   // Configure one or more authentication providers
+//   providers: [
+//     GithubProvider({
+//       clientId: process.env.GITHUB_ID,
+//       clientSecret: process.env.GITHUB_SECRET,
+//     }),
+//     // ...add more providers here
+//   ],
+// }
+
+// export default NextAuth(authOptions)
+
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import clientPromise from "@/lib/mongodb";
+
+export default NextAuth({
+  adapter: MongoDBAdapter(clientPromise),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+});
